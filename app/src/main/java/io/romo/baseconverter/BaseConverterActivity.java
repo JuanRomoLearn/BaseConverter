8ca99224cc46c3fdbc0beb32a32c3d108bb12431
Juan Romo <juanromolearn@gmail.com>
2017-12-21 22:18:44 -0600

    Initial commit

---
+++ app/src/main/java/io/romo/baseconverter/BaseConverterActivity.java
@@ -0,0 +1,13 @@
+package io.romo.baseconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_converter);
+    }
+}
