1439f780ec058e037c953c11695c6f4dc6b9165b
Juan Romo <juanromolearn@gmail.com>
2017-12-21 22:38:01 -0600

    Base converter layout completed

--- app/src/main/java/io/romo/baseconverter/BaseConverterActivity.java
+++ app/src/main/java/io/romo/baseconverter/BaseConverterActivity.java
@@ -2,12 +2,19 @@ package io.romo.baseconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class BaseConverterActivity extends AppCompatActivity {
+
+    private EditText decimalNumberInput;
+    private EditText binaryNumberInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_converter);
+
+        decimalNumberInput = findViewById(R.id.decimal_number_input);
+        binaryNumberInput = findViewById(R.id.binary_number_input);
     }
 }
